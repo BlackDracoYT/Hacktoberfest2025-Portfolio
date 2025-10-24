d9c42cde163749d05d2f568dd2cde71509c67d9b
Aaryan Raj <87359124+BlackDracoYT@users.noreply.github.com>
2025-10-25 00:29:54 +0530

    Add Fibonacci series program in C
    Implement Fibonacci series generation.

---
+++ FibonacciSeries.c
@@ -0,0 +1,19 @@
+  #include <stdio.h>
+
+int main() {
+   , next;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
+``
